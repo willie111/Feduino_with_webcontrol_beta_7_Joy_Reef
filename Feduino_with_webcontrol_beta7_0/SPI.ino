1a1b62e5699daa286a58d15b2c10564a5fa11503
Willie McDaries <wmcdaries3@gmail.com>
2015-04-19 18:04:00 -0400

    Update SPI.ino

--- Feduino_with_webcontrol_beta7_0/SPI.ino
+++ Feduino_with_webcontrol_beta7_0/SPI.ino
@@ -2,29 +2,29 @@ void selecionar_SPI(int dispositivo)
 {
   switch(dispositivo)
   {
-  case 0: // Utilizar cartÃƒÂ£o sd.
+  case 0: // Utilizar cartão sd.
     digitalWrite(ChipSelect_SD, LOW);
     digitalWrite(SelectSlave_ETH, HIGH);
     digitalWrite(ChipSelect_RFM, HIGH);
+    delay(10);
     break; 
 
   case 1: // Utilizar ethercard
     digitalWrite(SelectSlave_ETH, LOW);
     digitalWrite(ChipSelect_SD, HIGH);
     digitalWrite(ChipSelect_RFM, HIGH);
+    delay(10);
     break; 
 
   case 2: // Utilizar RFM12B
     digitalWrite(ChipSelect_RFM, LOW);
     digitalWrite(SelectSlave_ETH, HIGH);
     digitalWrite(ChipSelect_SD, HIGH);
+    delay(10);
     break; 
   }
   return;
 }
-
-
-
 
 
 
